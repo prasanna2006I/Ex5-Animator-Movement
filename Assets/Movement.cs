c3b3ec09ad3d19666fc53159cb0cb1ff5f57a96b
Prasanna I <150161282+prasanna2006I@users.noreply.github.com>
2025-11-18 17:29:49 +0530

    Add files via upload

---
+++ Assets/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Animator animator;
+    public float InputX;
+    public float InputY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator=this.gameObject.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputY = Input.GetAxis("Vertical");
+        animator.SetFloat("InputX", InputX);
+        animator.SetFloat("InputY", InputY);
+        
+    }
+}
